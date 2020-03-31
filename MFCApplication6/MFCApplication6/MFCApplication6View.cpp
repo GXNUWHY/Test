513781153d7dec4899e810006b2b1e50308b7a6f
GXNUWHY <1183448845@qq.com>
2020-03-31 12:51:13 +0800

    Revert "椭圆图案生成（未实现椭圆旋转）"
    This reverts commit 8acf2f2c2c5bc3a8add8f74ca92e18e77d4fc0a6.

--- MFCApplication6/MFCApplication6/MFCApplication6View.cpp
+++ MFCApplication6/MFCApplication6/MFCApplication6View.cpp
@@ -24,7 +24,6 @@ IMPLEMENT_DYNCREATE(CMFCApplication6View, CView)
 
 BEGIN_MESSAGE_MAP(CMFCApplication6View, CView)
 	ON_COMMAND(ID_MIDPOINTELLIPSE, &CMFCApplication6View::OnMidpointellipse)
-	ON_COMMAND(ID_32775, &CMFCApplication6View::OnPic1)
 END_MESSAGE_MAP()
 
 // CMFCApplication6View 构造/析构
@@ -88,7 +87,7 @@ void CMFCApplication6View::OnMidpointellipse()
 {
 	// TODO: 在此添加命令处理程序代码
 	CDC* pDC = GetDC();
-	int a = 200, b = 100, xc = 0, yc = 0;
+	int a = 200, b = 100, xc = 300, yc = 300;
 	int c = RGB(0, 0, 0);
 	double d1, d2;
 	int x, y;
@@ -131,73 +130,4 @@ void CMFCApplication6View::OnMidpointellipse()
 		pDC->SetPixel(x + 300, -y + 200, c);
 		pDC->SetPixel(-x + 300, -y + 200, c);
 	}
-}
-
-void CMFCApplication6View::Midpointellipse(int a, int b, int xc, int yc)
-{
-	// TODO: 在此添加命令处理程序代码
-	CDC* pDC = GetDC();
-	int c = RGB(0, 0, 0);
-	double d1, d2;
-	int x, y;
-	x = 0;
-	y = b;
-
-	
-	d1 = b * b + a * a * (-b + 0.25);
-	pDC->SetPixel(x + xc, y + yc, c);
-	pDC->SetPixel(-x + xc, y + yc, c);
-	pDC->SetPixel(x + xc, -y + yc, c);
-	pDC->SetPixel(-x + xc, -y + yc, c);
-	while (b * b * (x + 1) < a * a * (y - 0.5)) {
-		if (d1 < 0) {
-			d1 += b * b * (2 * x + 3);
-			++x;
-		}
-		else {
-			d1 += b * b * (2 * x + 3) + a * a * (-2 * y + 2);
-			++x;
-			--y;
-		}
-		pDC->SetPixel(x + xc, y + yc, c);
-		pDC->SetPixel(-x + xc, y + yc, c);
-		pDC->SetPixel(x + xc, -y + yc, c);
-		pDC->SetPixel(-x + xc, -y + yc, c);
-	}
-
-	d2 = sqrt(b * (x + 0.5)) + a * (y - 1) - a * b;
-	while (y > 0) {
-		if (d2 < 0) {
-			d2 += b * b*(2 * x + 2) + a * a*(-2 * y + 3);
-			++x;
-			--y;
-		}
-		else {
-			d2 += a * a*(-2 * y + 3);
-			--y;
-		}
-		pDC->SetPixel(x + xc, y + yc, c);
-		pDC->SetPixel(-x + xc, y + yc, c);
-		pDC->SetPixel(x + xc, -y + yc, c);
-		pDC->SetPixel(-x + xc, -y + yc, c);
-	}
-}
-
-void CMFCApplication6View::OnPic1()
-{
-	// TODO: 在此添加命令处理程序代码
-	int a = 100, b = 200;
-	int xc = 300, yc = 200;
-
-	for (a = 170, b = 100; a > 160; a -= 1, b -= 1) {
-		Midpointellipse(a, b, xc, yc);
-	}
-	for (a = 50, b = 100; a > 40; a -= 1, b -= 1) {
-		Midpointellipse(a, b, xc, yc);
-	}
-
-	for (a = 130, b = 50; a > 120; a -= 1, b -= 1) {
-		Midpointellipse(a, b, xc, yc - 50);
-	}
-	
 }
